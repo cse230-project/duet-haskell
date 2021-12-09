ccea1092c0236cbfbe55accd9d53a87e6977211f
peizhenwu <32442789+peizhenwu@users.noreply.github.com>
2021-12-09 12:43:54 -0800

    update tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -9,12 +9,12 @@ import qualified Model.Score as Score
 -- test Model/Board.hs
 testClockwise = TestCase(assertEqual "clockwise" (Board.Pos 34 15) (Board.clockwise (Board.Pos 35 15)))
 testCClockwise = TestCase(assertEqual "cClockwise" (Board.Pos 35 15) (Board.clockwise (Board.Pos 36 15)))
-testUp = TestCase(assertEqual "up" [Board.Pos 9 10, Board.Pos 19 20] (Board.up [Board.Pos 11 10, Board.Pos 21 20]))
+testUp = TestCase(assertEqual "up" [Board.Pos 9 10, Board.Pos 19 20] (Board.up [Board.Pos 11 10, Board.Pos 21 20] 2))
 testDown = TestCase(assertEqual "down" [Board.Pos 11 10, Board.Pos 21 20] (Board.down [Board.Pos 10 10, Board.Pos 20 20]))
 testRReset = TestCase(assertEqual "rreset" True (Board.redReset (Board.Pos 35 35)))
 testBReset = TestCase(assertEqual "breset" True (Board.blueReset (Board.Pos 35 15)))
-testCheck1 = TestCase(assertEqual "check1" True (Board.check [Board.Pos 30 1](Board.Pos 30 16)(Board.Pos 40 34)))
-testCheck2 = TestCase(assertEqual "check2" False (Board.check [Board.Pos 30 23](Board.Pos 30 16)(Board.Pos 40 34)))
+testCheck1 = TestCase(assertEqual "check1" True (Board.check [Board.Pos 30 1](Board.Pos 30 16)))
+testCheck2 = TestCase(assertEqual "check2" False (Board.check [Board.Pos 30 23](Board.Pos 30 16)))
 
 -- test Model/Score.hs
 testAddScore = TestCase(assertEqual "add" (Score.Score 1 1) (Score.add (Score.Score 0 0) 8))
